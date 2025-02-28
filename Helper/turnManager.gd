0d4932af34abda3663e1f5c1804c6d3045351082
Deogenes Maranan <123554998+DeogenesMaranan@users.noreply.github.com>
2025-02-28 23:47:36 +0800

    Simplify turnManager

--- Helper/turnManager.gd
+++ Helper/turnManager.gd
@@ -2,25 +2,19 @@ extends Node
 
 signal turn_switched(current_player: RigidBody2D)
 
-@onready var player1: RigidBody2D = $"../Player1"
-@onready var player2: RigidBody2D = $"../Player2"
+@onready var players: Array[RigidBody2D] = [$"../Player1", $"../Player2"]
 @onready var joystick: Node2D = $"../HUD/BotCenter/Joystick"
 @onready var turn_label: Label = $"../HUD/TopCenter/CurrentPlayer"
-@onready var players: Array[RigidBody2D] = [player1, player2]
-@onready var current_player: RigidBody2D
+@onready var current_player: RigidBody2D = players[0]
 
 var tween: Tween
 
 func _ready():
-	if players.size() > 0:
-		current_player = players[0]
-		_update_turn_label()
-		_highlight_current_player()
+	_update_turn_label()
+	_highlight_current_player()
 
 func switch_turn():
-	var current_index = players.find(current_player)
-	current_index = (current_index + 1) % players.size()
-	current_player = players[current_index]
+	current_player = players[(players.find(current_player) + 1) % players.size()]
 	emit_signal("turn_switched", current_player)
 	_update_turn_label()
 	_highlight_current_player()
@@ -30,32 +24,26 @@ func get_current_player() -> RigidBody2D:
 
 func set_players(new_players: Array[RigidBody2D]):
 	players = new_players
-	if players.size() > 0:
-		current_player = players[0]
-		_update_turn_label()
-		_highlight_current_player()
+	current_player = players[0]
+	_update_turn_label()
+	_highlight_current_player()
 
 func _update_turn_label():
-	if turn_label:
-		turn_label.text = "Player 1's Turn" if current_player == players[0] else "Player 2's Turn"
+	turn_label.text = "Player 1's Turn" if current_player == players[0] else "Player 2's Turn"
 
 func _highlight_current_player():
 	for player in players:
-		player.get_node("Sprite2D").modulate = Color(1, 1, 1, 0.5)  # Dim all players
-	current_player.get_node("Sprite2D").modulate = Color.WHITE  # Highlight the current player
+		player.get_node("Sprite2D").modulate = Color(1, 1, 1, 0.5)
+	current_player.get_node("Sprite2D").modulate = Color.WHITE
 
 func start_fade(target_color: Color):
 	if tween and tween.is_running():
 		tween.kill()
-	
 	tween = get_tree().create_tween()
 	tween.tween_property(joystick, "modulate", target_color, 0.3).set_trans(Tween.TRANS_QUAD).set_ease(Tween.EASE_OUT)
 
 func are_balls_moving() -> bool:
-	for player in players:
-		if player.linear_velocity.length() > 5:
-			return true
-	return false
+	return players.any(func(p): return p.linear_velocity.length() > 5)
 
 func process_turn_state():
 	if are_balls_moving():
--- main.gd
+++ main.gd
@@ -6,16 +6,11 @@ extends Node2D
 func _ready():
 	var viewport_rect = get_viewport_rect()
 	wall_generator.generate_walls(viewport_rect)
-	turn_manager.turn_switched.connect(_on_turn_switched)
-
+	
 func _on_joystick_input_received(input_vector: Vector2):
 	if not turn_manager.are_balls_moving():
-		print(turn_manager.get_current_player())
 		turn_manager.get_current_player().apply_input(input_vector)
 		turn_manager.switch_turn()
-
-func _on_turn_switched(_current_player: RigidBody2D):
-	pass
 
 func _process(_delta: float):
 	turn_manager.process_turn_state()
